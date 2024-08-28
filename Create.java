bcd03df2a08347cf85b4782400bb7d472c9ea01d
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-28 18:27:01 +0530

    create

--- Create.java
+++ Create.java
@@ -1,7 +1,19 @@
+import java.util.*;
 public class Create{
-public static void main(String[]args){
+
 // Create an Object for ArrayList
   // Load Values in ArrayList
   // Iterate it by usig Iterator and While Loops
+  public static void main(String[]args){
+  int[]a=new int[5];
+List<Integer> l = new ArrayList<Integer>();
+      
+      l.add(3);
+      l.add(4);
+     
+        Iterator k = l.iterator();
+        while (k.hasNext()) {
+            System.out.println(k.next());
+        }
 }
 }
