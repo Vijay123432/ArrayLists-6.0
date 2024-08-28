e336b9e7e4ef0dded5f81148eaf7a748b94bdb98
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-28 18:44:23 +0530

    forl

--- ForL.java
+++ ForL.java
@@ -1,7 +1,17 @@
+import java.util.*;
 public class ForL{
 public static void main(String[]args){
 // prepare an object for ArrayList
   // Load String instead of Integers 
   // Iterate them using for each loop
+  List<String> l=new ArrayList<String>();
+
+  l.add(0,"marsh");
+  l.add(1,"mello");
+
+  Iterator t=l.iterator();
+  while(t.hasNext()){
+System.out.println(t.next());
+  }
 }
 }
